d4eaf091b8f3489d7d25f4ca352cd8bcd15cd59c
Denis <dikan-2002@mail.ru>
2024-11-27 01:47:47 +0400

    closes #1: created data classes

---
+++ src/classes/User.java
@@ -0,0 +1,85 @@
+package classes;
+
+public class User implements Comparable<User>{
+
+    private String userName;
+    private String password;
+    private String email;
+
+    private User(UserBuilder userBuilder) {
+        this.userName = userBuilder.userName;
+        this.password = userBuilder.password;
+        this.email = userBuilder.email;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public static class UserBuilder {
+        private String userName;
+        private String password;
+        private String email;
+
+
+        public UserBuilder setUserName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public UserBuilder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User build(){
+            return new User(this);
+        }
+
+    }
+
+    @Override
+    public int compareTo(User o) {
+        if(this.userName != null && o.userName != null){
+            int compareUserName = this.userName.compareTo(o.userName);
+            if (compareUserName != 0) {
+                return compareUserName;
+            }
+        }
+
+        if(this.password != null && o.password != null){
+            int comparePassword = this.password.compareTo(o.password);
+            if (comparePassword != 0) {
+                return comparePassword;
+            }
+        }
+
+        if(this.email != null && o.email != null){
+            return this.email.compareTo(o.email);
+        }
+
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", password=" + password +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
