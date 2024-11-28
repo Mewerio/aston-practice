47149d4d1ddce5bc0099ff2dd08d6ec2d7dab8b0
Grigory Pishchula <muzurti94@gmail.com>
2024-11-28 20:39:19 +0400

    Add files via upload

--- src/interfaces/InputData.java
+++ src/interfaces/InputData.java
@@ -1,6 +1,7 @@
 package interfaces;
 import java.util.List;
+import java.util.Scanner;
 
 public interface InputData {
-    List<Comparable> getData();
+    List<Comparable> getData(Scanner scanner, int length);
 }
