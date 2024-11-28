b1846a88cf3f8a4650e43d468d0e516210b312ab
Grigory Pishchula <muzurti94@gmail.com>
2024-11-29 01:00:22 +0400

    Add files via upload

--- src/classes/UserInput.java
+++ src/classes/UserInput.java
@@ -7,44 +7,45 @@ import java.util.Scanner;
 public class UserInput implements InputData {
 
     @Override
-    public List<Comparable> getData(Scanner scanner, int length) {
+    public List<Comparable> getData(Scanner scanner) {
 
+        int length = scanner.nextInt();
+        scanner.nextLine();
         List<Comparable> list = new ArrayList<>();
 
-        for (int i=0; i<length; i++) {
+        try {
+            for (int i = 0; i < length; i++) {
 
-            String[] str = scanner.next().split(",");
+                String[] str = scanner.nextLine().split(",");
+                String classType = str[0];
+                String firstVar = str[1];
+                String secondVar = str[2];
+                String thirdVar = str[3];
 
-            if (str.length < 4) {
-                System.out.println("Данные введены с ошибками");
-                break;
+                if (classType.equalsIgnoreCase("bus") && Integer.parseInt(thirdVar) >= 0) {
+                    Bus bus = new Bus.BusBuilder().setNumber(firstVar)
+                            .setModel(secondVar).setMileage(Integer.parseInt(thirdVar)).build();
+                    list.add(bus);
+                } else if (classType.equalsIgnoreCase("user")) {
+                    User user = new User.UserBuilder().setUserName(firstVar)
+                            .setPassword(secondVar).setEmail(thirdVar).build();
+                    list.add(user);
+                } else if (classType.equalsIgnoreCase("student")
+                        && Double.parseDouble(secondVar) >= 2.0 && Double.parseDouble(secondVar) <= 5.0) {
+                    Student student = new Student.StudentBuilder().setGroupNumber(firstVar).
+                            setAverageScore(Double.parseDouble(secondVar))
+                            .setStudentIdCard(Integer.parseInt(thirdVar)).build();
+                    list.add(student);
+                } else {
+                    System.out.println("Данные введены с ошибками");
+                    break;
+                }
+
             }
+        }
 
-            else if (str[0].equalsIgnoreCase("bus") && Integer.parseInt(str[3]) >=0) {
-                Bus bus = new Bus.BusBuilder().setNumber(str[1])
-                        .setModel(str[2]).setMileage(Integer.parseInt(str[3])).build();
-                list.add(bus);
-            }
-
-            else if (str[0].equalsIgnoreCase("user")) {
-                User user = new User.UserBuilder().setUserName(str[1])
-                        .setPassword(str[2]).setEmail(str[3]).build();
-                list.add(user);
-            }
-
-            else if (str[0].equalsIgnoreCase("student")
-                    && Double.parseDouble(str[2])>=2.0 && Double.parseDouble(str[2])<=5.0) {
-                Student student = new Student.StudentBuilder().setGroupNumber(str[1]).
-                        setAverageScore(Double.parseDouble(str[2]))
-                        .setStudentIdCard(Integer.parseInt(str[3])).build();
-                list.add(student);
-            }
-
-            else {
-                System.out.println("Данные введены с ошибками");
-                break;
-            }
-
+        catch (Exception e) {
+            System.out.println("Данные введены с ошибками");
         }
 
         if (list.size() != length) return null;
