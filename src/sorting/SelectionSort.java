377803dee32be41cf9e56ab075bb739f02eefa6c
Denis <dikan-2002@mail.ru>
2024-11-28 14:06:57 +0400

    Closes #4: created class for selection sort

---
+++ src/sorting/SelectionSort.java
@@ -0,0 +1,31 @@
+package sorting;
+
+import interfaces.SortingStrategy;
+
+import java.util.List;
+
+public class SelectionSort implements SortingStrategy {
+
+    @Override
+    public <T extends Comparable<T>> void sort(List<T> list) {
+        int n = list.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (list.get(j).compareTo(list.get(minIndex)) < 0) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                T temp = list.get(i);
+                list.set(i, list.get(minIndex));
+                list.set(minIndex, temp);
+            }
+        }
+    }
+
+
+}
